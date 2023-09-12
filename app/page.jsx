b49e793027d3d48cb888679e08a43944808446a2
Valarie Oyieke <oyiekeav@gmail.com>
2023-09-12 08:49:26 +0300

    homepage fixes

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,8 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from './styles/page.module.css'
-import Hero from './hero/page'
+import Hero from './components/Hero'
+
 
 export const metadata = {
   title: 'Dala Afrika | Home',
@@ -10,26 +11,27 @@ export const metadata = {
 export default function Home() {
   return (
     <main className={styles.main}>
-       <Hero />
+       <Hero /> 
        <div className={styles.about}>
           <h2>What We Do</h2>
         <div className={styles.cards}>
             <div className={styles.card}>
-              <h3>Education</h3>
+              <h3>Educate</h3>
+              <p>We offer a variety of educational resources on blockchain and Web3 technologies</p>
             </div>
 
             <div className={styles.card}>
-              <h3>Collaboration</h3>
+              <h3>Collaborate</h3>
+              <p>We collaborate with blockchain projects and initiatives to help them get into the African market.</p>
             </div>
 
             <div className={styles.card}>
-              <h3>Discussion</h3>
+              <h3>Advocate</h3>
+              <p>We advocate for the adoption of Web3 technologies in Africa.</p>
             </div>
-
-            <div className={styles.card}>
-              <h3>Advocacy</h3>
-            </div>
+            
         </div>
+        <button className={styles.moreBtn}>FIND OUT MORE</button>
        </div>
     </main>
   )
